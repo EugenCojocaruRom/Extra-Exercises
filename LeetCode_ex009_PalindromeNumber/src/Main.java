3fe4655b8a7fba902d73010bdf0607a6c9c3316e
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-04 22:53:31 +0200

    LeetCode - exercise 009 - palindrome number
    Given an integer x, return 'true' if x is a palindrome, and 'false' otherwise.

---
+++ LeetCode_ex009_PalindromeNumber/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Open keyboard
+        Scanner sc = new Scanner(System.in);
+        //Display value x
+        System.out.print("x = ");
+        //Declare variable 'x' of type int and read it from the keyboard
+        double x = sc.nextDouble();
+        //Declare boolean 'isPalindrome' and set its default value to 'true'
+        boolean isPalindrome = true;
+        //Covert the number to String to be able to check its digits
+        String str = Double.toString(x);
+        //Declare variable 'n' as the length of String 'str'
+        int n = str.length();
+        //Loop through the String 'str' from i = 0 to half of the length of 'str', incrementing i by 1;
+        for (int i = 0; i < n / 2; ++i) {
+            /*Set the condition for comparing the characters of the string
+            starting from i from one end and with i-1 from the other end*/
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                isPalindrome = false;
+            } else {
+                isPalindrome = true;
+            }
+        }
+
+        if (isPalindrome) {
+            //Display message that the number is a palindrome
+            System.out.print(true);
+        } else {
+            //Display message that the number is not a palindrome
+            System.out.print(false);
+        }
+    }
+}
