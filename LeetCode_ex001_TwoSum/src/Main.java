37c2a7cfbcc1558b19025681e852a17162014aca
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-05 09:31:39 +0200

    some changes in the comments
    Some modifications to the comments:
    1. added 'example <number>' in the comment for each case, including for the commented out cases
    2. added 'example <number>' as comment for the 2 commented out result cases

--- LeetCode_ex001_TwoSum/src/Main.java
+++ LeetCode_ex001_TwoSum/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         //Declare array of type int with fixed values
-        int[] nums = {2, 7, 11, 15}; // --> target = 9
-//        int[] nums = {3, 2, 4, 5}; // --> target = 6
-//        int[] nums = {45, 3, 8, -12}; // --> target = -4
+        int[] nums = {2, 7, 11, 15}; // --> example 1: target = 9
+//        int[] nums = {3, 2, 4, 5}; // --> example 2: target = 6
+//        int[] nums = {45, 3, 8, -12}; // --> example 3: target = -4
         //Declare variable n equal to nums.length
         int n = nums.length;
         //Loop through array nums from i = 0 to n, incrementing i by 1
@@ -15,8 +15,8 @@ public class Main {
                 int result = nums[i] + nums[j];
                 //Set the rule for calculating the target value
                 if (result == 9) {
-//                if (result == 6) {
-//                if (result == -4) {
+//                if (result == 6) { // --> example 2
+//                if (result == -4) { // --> example 3
                     //Display the indices of the 2 numbers that add up to the value of the target
                     System.out.print("[" + i + ", " + j + "]");
                 }
