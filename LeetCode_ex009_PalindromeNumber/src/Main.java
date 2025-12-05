d392d31efdf8b6578a2d7d2721e6ba5be7f12c81
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-05 09:26:13 +0200

    some changes in the code
    1. changed the data type to 'long' to accept larger numbers; modified the data type in the comment for the line as well
    2. changed the wrapper to Long for the conversion to String
    3. removed 'else' and added 'break' to stop the check in case of 'false' return

--- LeetCode_ex009_PalindromeNumber/src/Main.java
+++ LeetCode_ex009_PalindromeNumber/src/Main.java
@@ -7,12 +7,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         //Display value x
         System.out.print("x = ");
-        //Declare variable 'x' of type int and read it from the keyboard
-        double x = sc.nextDouble();
+        //Declare variable 'x' of type long and read it from the keyboard
+        long x = sc.nextLong();
         //Declare boolean 'isPalindrome' and set its default value to 'true'
         boolean isPalindrome = true;
-        //Covert the number to String to be able to check its digits
-        String str = Double.toString(x);
+        //Set condition for the number entered to not be less than 0
+        if (x < 0) {
+            //Return 'false' if the number is less than 0
+            isPalindrome = false;
+        }
+        //Convert the number to String to be able to check its digits
+        String str = Long.toString(x);
         //Declare variable 'n' as the length of String 'str'
         int n = str.length();
         //Loop through the String 'str' from i = 0 to half of the length of 'str', incrementing i by 1;
@@ -21,8 +26,7 @@ public class Main {
             starting from i from one end and with i-1 from the other end*/
             if (str.charAt(i) != str.charAt(n - i - 1)) {
                 isPalindrome = false;
-            } else {
-                isPalindrome = true;
+                break;
             }
         }
 
