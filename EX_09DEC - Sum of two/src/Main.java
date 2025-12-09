6944e418fd0cf1a394f06d9733a37bcf993bc515
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-09 11:32:00 +0200

    Added comments and deleted useless variable
    1. Added comments for each line
    2. Deleted int sum, as it was unnecessary; added the method in the message

--- EX_09DEC - Sum of two/src/Main.java
+++ EX_09DEC - Sum of two/src/Main.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Open keyboard
         Scanner sc = new Scanner(System.in);
+        //Display value a for the first number
         System.out.print("a = ");
+        //Declare variable of type int and read it from the keyboard
         int a = sc.nextInt();
+        //Display value b for the second number
         System.out.print("b = ");
+        //Declare variable of type int and read it from the keyboard
         int b = sc.nextInt();
 
-        int sum = sumOfTwo(a, b);
-        System.out.print("The sum of a and b is " + sum);
+        //Display the result of the addition, containing the result called by the method
+        System.out.print("The sum of a and b is " + sumOfTwo(a, b));
     }
 
+    //Define the method for calculating the sum of the 2 numbers
     static int sumOfTwo(int a, int b) {
+        //Return the sum of the 2 numbers
         return a + b;
     }
 }
