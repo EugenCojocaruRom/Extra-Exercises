ff456e7fe60d89c0d10a9bb17580193dba8f6cae
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-21 08:48:13 +0200

    added comments
    Added comments for several lines of code

--- EX_20DEC - CodingBat_ex05 - old35/src/Main.java
+++ EX_20DEC - CodingBat_ex05 - old35/src/Main.java
@@ -16,12 +16,17 @@ public class Main {
 
     //Define method for checking if n is a multiple of 3 or 5
     static boolean isMultipleOf3Or5(int n) {
+        //Set condition for checking that n is only a multiple of 3
         if ((n % 3 == 0) && (n % 5 != 0)) {
+            //Return true if the condition is satisfied
             return true;
         }
+        //Set condition for checking that n is only a multiple of 5
         else if ((n % 3 != 0) && (n % 5 == 0)) {
+            //Return true if the condition is satisfied
             return true;
         }
+        //Return false if the none of the conditions above is satisfied
         return false;
     }
 }
