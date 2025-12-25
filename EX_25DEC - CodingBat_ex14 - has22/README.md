dfc09ba5482ffdd9632a450eeddf4794fb1804f4
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2025-12-25 07:46:28 +0200

    EX_25DEC - CodingBat_ex14 - has22
    Return true if a given array contains two consecutive 2's.

---
+++ EX_25DEC - CodingBat_ex14 - has22/README.md
@@ -0,0 +1,4 @@
+EX\_25DEC - CodingBat\_ex14 - has22
+
+Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+
---
+++ EX_25DEC - CodingBat_ex14 - has22/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Open keyboard
+        Scanner sc = new Scanner(System.in);
+        //Display value nums for the number of elements in the array
+        System.out.print("nums = ");
+        //Declare variable 'nums' of type int and read it from the keyboard
+        int nums = sc.nextInt();
+        //Declare array A of type int and set the number of its elements to the value of nums
+        int[] A = new int[nums];
+
+        //Method for reading array A
+        readArrayA(A, sc);
+        //Method for finding if there are 2 consecutive values of 2
+        System.out.println("The array contains 2 consecutive 2's: " + has22(A));
+    }
+
+    //Define the method for reading array A
+    static void readArrayA(int[] A, Scanner sc) {
+        //Loop through array A from i = 0 to the length of A, incrementing i by 1
+        for (int i = 0; i < A.length; i++) {
+            //Display each element of array A
+            System.out.print("A[" + i + "] = ");
+            //Read the value of each element from the keyboard
+            A[i] = sc.nextInt();
+        }
+        //Display array A
+        System.out.println("Array A is: " + Arrays.toString(A));
+    }
+
+    //Define the method for finding if there are 2 consecutive values of 2
+    static boolean has22(int[] A) {
+        //Loop through array A from i = 0 to the length of A, incrementing i by 1
+        for (int i = 0; i < A.length; i++) {
+            //Set the condition for checking if there are 2 consecutive 2's
+            if (A[i] == 2 && A[i + 1] == 2) {
+                //Return true if the condition is satisfied
+                return true;
+            }
+        }
+        //Return false otherwise
+        return false;
+    }
+}
