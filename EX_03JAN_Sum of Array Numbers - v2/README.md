ee3eca85b71417c78b70062e02b3cabab41e52c3
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-01-03 09:04:21 +0200

    EX_03JAN_Sum of Array Numbers - v2
    Compute the sum of all the numbers of an array until the first negative one is found (excluding it).
    Version with 'for-each' loop

---
+++ EX_03JAN_Sum of Array Numbers - v2/README.md
@@ -0,0 +1,8 @@
+EX\_03JAN\_Sum of Array Numbers - v2
+
+Given an array of numbers, compute the sum of all numbers until first negative one is found (excluding it).
+
+
+
+\*\*NOTE: Version with 'for-each' loop.
+
---
+++ EX_03JAN_Sum of Array Numbers - v2/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Open keyboard
+        Scanner sc = new Scanner(System.in);
+        //Display value 'n' for the number of elements in the array
+        System.out.print("n = ");
+        //Declare variable 'n' of type int and read it from the keyboard
+        int n = sc.nextInt();
+        //Declare array A of type double and set the number of its elements to n
+        double[] A = new double[n];
+
+        //Method for reading and displaying array A
+        displayA(A, sc);
+        //Method for displaying the sum
+        System.out.println("The sum is: " + sumPositives(A));
+    }
+
+    //Define method for reading and displaying array A
+    static void displayA(double[] A, Scanner sc) {
+        //Loop through array A from i = 0 to the length of A, incrementing i by 1
+        for (int i = 0; i < A.length; i++) {
+            //Display the label for each element of array A
+            System.out.print("A[" + i + "] = ");
+            //Read each element of array A from the keyboard
+            A[i] = sc.nextDouble();
+        }
+        //Display the elements of the original array
+        System.out.println("The elements of array A are: " + Arrays.toString(A));
+    }
+
+    //Define method for displaying the sum of the positive elements from array A
+    static double sumPositives(double[] A) {
+        //Declare variable 'sum' of type double and initialize it
+        double sum = 0;
+        //Loop through the values of the array
+        for (double valA : A) {
+            //Set the condition for checking if the element is positive
+            if (valA < 0) {
+                //Stop the loop when a negative value is fount
+                break;
+            }
+            //Add the element (if positive) to the sum at each iteration
+            sum += valA;
+        }
+        //Return the sum
+        return sum;
+    }
+}
