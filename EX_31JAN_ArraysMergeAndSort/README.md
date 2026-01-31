29bc2e866d87c6e35b839cc8559c623bfade1f60
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-01-31 22:27:40 +0200

    EX\_31JAN - Merge arrays and sort
    Create a method named 'merge' that receives two arrays as arguments. The method merges the two arrays into one sorted array and returns it.
    The final merged array must be sorted in ascending order.

---
+++ EX_31JAN_ArraysMergeAndSort/README.md
@@ -0,0 +1,6 @@
+EX\_31JAN - Merge arrays and sort
+
+Create a method named 'merge' that receives two arrays as arguments. The method merges the two arrays into one sorted array and returns it.
+
+The final merged array must be sorted in ascending order.
+
---
+++ EX_31JAN_ArraysMergeAndSort/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Open keyboard
+        Scanner scanner = new Scanner(System.in);
+        //Display prompt
+        System.out.print("Elements of array 1 (separated by comma): ");
+        //Declare variable of type String and read it from the keyboard
+        String textArr1 = scanner.nextLine();
+        //Display prompt
+        System.out.print("Elements of array 2 (separated by comma): ");
+        //Declare variable of type String and read it from the keyboard
+        String textArr2 = scanner.nextLine();
+        //Declare array of type String and split its elements by comma
+        String[] arr1 = textArr1.split(",");
+        //Declare array of type String and split its elements by comma
+        String[] arr2 = textArr2.split(",");
+
+        //Declare array of type string (array 1 merges with array 2) and call the 'merge' method
+        String[] mergedArray = merge(arr1, arr2);
+        //Display the merged array
+        System.out.println(Arrays.toString(mergedArray));
+    }
+
+    //Define method for merging the arrays
+    static String[] merge(String[] arr1, String[] arr2) {
+        //Declare array of type String and set the number of its elements as the sum of the elements of the other 2 arrays
+        String[] arr3 = new String[arr1.length + arr2.length];
+        //Copy the elements of array 1 into array 3
+        System.arraycopy(arr1, 0, arr3, 0, arr1.length);
+        //Copy the elements of array 2 into array 3, after the elements of array 1
+        System.arraycopy(arr2, 0, arr3, arr1.length, arr2.length);
+        //Sort the elements of array3 using the Comparator class
+        Arrays.sort(arr3, new Comparator<String>() {
+            @Override
+            //Use compare() method for comparing the elements of the merged array, to sort them in the correct order
+            public int compare(String o1, String o2) {
+                //Return the result of the comparison between 2 elements of the array
+                return Integer.compare(Integer.parseInt(o1), Integer.parseInt(o2));
+            }
+        });
+        //Return the sorted merged array
+        return arr3;
+    }
+}
