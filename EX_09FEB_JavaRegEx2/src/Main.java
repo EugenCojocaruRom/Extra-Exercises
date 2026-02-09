033b616116ca9c5840888a29cf8546d44833e7dd
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-02-09 17:15:13 +0200

    updated code
    Made some updates in the code

--- EX_09FEB_JavaRegEx2/src/Main.java
+++ EX_09FEB_JavaRegEx2/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,12 +14,12 @@ public class Main {
         //Display prompt for user - enter sentence
         System.out.println("Enter sentences:");
         //Set condition for reading each sentence from the keyboard
-        List<Map<String, Integer>> result = new ArrayList<>();
+        List<Map<Integer, String>> result = new ArrayList<>();
         while (n != 0) {
             int index = 0;
             //Declare variable of type String and read it from the keyboard
             String[] words = sc.nextLine().split(" ");
-            Map<String, Integer> seen = new HashMap<>();
+            Map<String, Integer> seen = new TreeMap<>();
             for (String word : words) {
                 String lowerCaseWord = word.toLowerCase();
                 if (!seen.containsKey(lowerCaseWord)) {
@@ -26,9 +27,29 @@ public class Main {
                 }
             }
             n--;
-            result.add(seen);
+            //Sort map by value, in ascending order
+//            Map<String, Integer> sorted_seen = seen.entrySet().stream()
+//                    .sorted(Map.Entry.comparingByValue())
+//                            .map(Map.Entry::getKey)
+//                                    .collect(Collectors.toList());
+
+            Map<Integer, String> sorted_seen = new TreeMap<>();
+            for (Map.Entry<String, Integer> entry : seen.entrySet()) {
+                sorted_seen.put(entry.getValue(), entry.getKey());
+            }
+
+            result.add(sorted_seen);
         }
-        System.out.println(result);
+        for (Map<Integer, String> row : result) {
+            //Sort map by value, in ascending order
+            //Collections.sort(Map.Entry.comparingByValue(row, (a, b) -> a.));
+            for (Map.Entry<Integer, String> entry : row.entrySet()) {
+
+                System.out.print(entry.getValue()+" ");
+            }
+            System.out.println();
+        }
+//        System.out.println(result);
     }
 
 
