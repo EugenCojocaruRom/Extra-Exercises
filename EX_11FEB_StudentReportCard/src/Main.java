6e7029ab636032d7a70be9ff7aa710383f130255
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-02-11 10:53:53 +0200

    small updates
    added comments and made some updates in the code

--- EX_11FEB_StudentReportCard/src/Main.java
+++ EX_11FEB_StudentReportCard/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
         double scienceGrade = sc.nextDouble(); //e.g. 88.7777
         sc.nextLine();
         //Display prompt for the user - English grade
-        System.out.print("Enter the Science grade: ");
+        System.out.print("Enter the English grade: ");
         //Declare variable of type double and read it from the keyboard
         double englishGrade = sc.nextDouble(); //e.g. 95.1111
         sc.nextLine();
@@ -44,16 +44,17 @@ public class Main {
         String gradesInfo = String.format("Math: %.2f, Science: %.2f, English: %.2f",
                 mathGrade, scienceGrade, englishGrade);
 
-        //Create the formatted string for student attendance
+        //Declare variable of type double and calculate the attendance as a 2 decimal floating number
         double attendancePercent = attendance * 100.0;
+        //Create the formatted string for student attendance
         String attendanceInfo = String.format("Attendance: %.1f%%", attendancePercent);
 
         //Display separator
-        System.out.println("------------------");
+        System.out.println("-------------------");
         //Display section title
         System.out.println("STUDENT REPORT CARD");
         //Display section separator
-        System.out.println("------------------");
+        System.out.println("-------------------");
         //Display the formatted strings
         System.out.println(headerInfo);
         System.out.println(gradesInfo);
@@ -61,14 +62,23 @@ public class Main {
     }
 
     static String capitalizedStudentName(String studentName) {
+        //Set condition for the cases when the student name is null or there is no value
         if (studentName == null || studentName.isBlank()) {
+            //Return empty string
             return "";
         }
-        String[] initialStudentName = studentName.split(" ");
+        /*Declare array of type String, trim any extra leading/trailing spaces
+        and split the initial student name by ' ' (space)
+            --> used \\s+ in case there are one or more whitespace characters*/
+        String[] initialStudentName = studentName.trim().split("\\s+");
+        //Declare variable of type String and initialize it as an empty string (to store the modified student name afterwards)
         String finalStudentName = "";
+        //Loop through the elements of the 'initialStudentName' array
         for (String name : initialStudentName) {
+            //Add to the 'finalStudentName' string the capitalized first letter of each element followed by the rest of the element in lowercase
             finalStudentName += name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + " ";
         }
+        //Return the modified student name (any extra spaces are trimmed)
         return finalStudentName.trim();
     }
 }
