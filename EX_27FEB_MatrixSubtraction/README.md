7b920528123b0d8feb9f07b96883ca294c951a8e
Eugen Cojocaru <eugen.cojocaru@gmail.com>
2026-02-27 10:51:52 +0200

    EX_27FEB - Matrix subtraction
    Create a method named 'subMatrices' that takes two 2D arrays (matrices) as input and return their difference as a new 2D array.

---
+++ EX_27FEB_MatrixSubtraction/README.md
@@ -0,0 +1,4 @@
+EX\_27FEB - Matrix subtraction
+
+Create a method named 'subMatrices' that takes two 2D arrays (matrices) as input and return their difference as a new 2D array.
+
---
+++ EX_27FEB_MatrixSubtraction/src/Main.java
@@ -0,0 +1,101 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Open keyboard
+        Scanner sc = new Scanner(System.in);
+        //Display prompt
+        System.out.print("Enter the number of rows: ");
+        //Declare variable of type int and read it from the keyboard
+        int n = sc.nextInt();
+        sc.nextLine();
+        //Display prompt
+        System.out.print("Enter the number of columns: ");
+        //Declare variable of type int and read it from the keyboard
+        int m = sc.nextInt();
+        sc.nextLine();
+        //Declare 2D arrays of type int and set the number of elements for rows to n and for columns to m
+        int[][] matrix1 = new int[n][m];
+        int[][] matrix2 = new int[n][m];
+
+        //Method for displaying the 2 matrices and their elements
+        printMatrix(matrix1, matrix2, sc);
+        //Display the resulting matrix after performing the subtraction
+        subMatrices(matrix1, matrix2);
+    }
+
+    //Define method for reading and displaying the matrices
+    static void printMatrix(int[][] matrix1, int[][] matrix2, Scanner sc) {
+        //Display section title for Matrix 1
+        System.out.println("\n<=== Matrix 1 ===>");
+        //Loop from i = 0 to length of the array, incrementing i by 1
+        for (int i = 0; i < matrix1.length; i++) {
+            //Display prompt for entering the elements for each row
+            System.out.println("Enter the elements for row " + (i +1) + ": ");
+            //Inside this loop --> loop from j = 0 to length of each row, incrementing j by 1
+            for (int j = 0; j < matrix1[i].length; j++) {
+                //Display label for element
+                System.out.print("Element " + (j + 1) + ": ");
+                //Read the element from the keyboard
+                matrix1[i][j] = sc.nextInt();
+                sc.nextLine();
+            }
+        }
+
+        //Display section title for Matrix 2
+        System.out.println("\n<=== Matrix 2 ===>");
+        //Loop from i = 0 to length of the array, incrementing i by 1
+        for (int i = 0; i < matrix2.length; i++) {
+            //Display prompt for entering the elements for each row
+            System.out.println("Enter the elements for row " + (i +1) + ": ");
+            //Inside this loop --> loop from j = 0 to length of each row, incrementing j by 1
+            for (int j = 0; j < matrix2[i].length; j++) {
+                //Display label for element
+                System.out.print("Element " + (j + 1) + ": ");
+                //Read the element from the keyboard
+                matrix2[i][j] = sc.nextInt();
+                sc.nextLine();
+            }
+        }
+
+        //Display matrix 1
+        System.out.println("\nMatrix 1 is: ");
+        //Loop through all the rows of the matrix
+        for (int[] row : matrix1) {
+            //Display each row on a separate line
+            System.out.println(Arrays.toString(row));
+        }
+        //Display matrix 2
+        System.out.println("\nMatrix 2 is: ");
+        //Loop through all the rows of the matrix
+        for (int[] row : matrix2) {
+            //Display each row on a separate line
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    //Define method for performing matrix subtraction
+    public static int[][] subMatrices(int[][] matrix1, int[][] matrix2) {
+        //Declare 2D array of type int and initialize it
+        int[][] resultMatrix = new int[matrix1.length][matrix1[0].length];
+        //Loop from i = 0 to length of the 'resultMatrix' array, incrementing i by 1
+        for (int i = 0; i < resultMatrix.length; i++) {
+            //Inside this loop --> loop from j = 0 to length of each row, incrementing j by 1
+            for (int j = 0; j < resultMatrix[i].length; j++) {
+                //Set the value of each element as the result of the subtraction operation
+                resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+        //Display label
+        System.out.println("\nMatrix after subtraction: ");
+        //Loop through all the rows of the matrix
+        for (int[] row : resultMatrix) {
+            //Display each row on a separate line
+            System.out.println(Arrays.toString(row));
+        }
+        //Return the resulting matrix
+        return resultMatrix;
+    }
+}
